d6bec7bd7fe93d4f87cd66b0ceddc03cf328dfca
Andrew Ghaly <andrewmghaly@gmail.com>
2020-12-08 13:01:01 -0500

    fixed context

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import TicTacToeContext from '../contexts/ticTacToeContext'
-import BoardRow from './boardRow';
+import React, { Component } from "react";
+import TicTacToeContext from "../contexts/ticTacToeContext";
+import BoardRow from "./boardRow";
 class Board extends Component {
-    state = {}
-    changeValue = () => {
-
+  state = {
+    nextValue: "x",
+  };
+  changeValue = () => {
+    const { nextValue } = this.state;
+    if (nextValue === "_") {
+      this.setState({ nextValue: "x" });
+    } else if (nextValue === "x") {
+      this.setState({ nextValue: "o" });
+    } else if (nextValue === "o") {
+      this.setState({ nextValue: "x" });
     }
-    render() {
-        return (
-            <TicTacToeContext.Provider value={{ nextValue: "x", changeValue: this.changeValue }}>
-                <table style={{
-                    margin: "auto",
-                    padding: "5px",
-                    marginTop: "150px", backgroundColor: "lightblue", width: "450px", height: "375px"
-                }}>
-                    <tbody>
-                        <BoardRow></BoardRow>
-                        <BoardRow isCenterRow={true}></BoardRow>
-                        <BoardRow></BoardRow>
-                    </tbody>
-                </table>
-            </TicTacToeContext.Provider>
-        );
-    }
+  };
+  render() {
+    const { nextValue } = this.state;
+    return (
+      <TicTacToeContext.Provider
+        value={{ nextValue, changeValue: this.changeValue }}
+      >
+        <table
+          style={{
+            margin: "auto",
+            padding: "5px",
+            marginTop: "150px",
+            backgroundColor: "lightblue",
+            width: "450px",
+            height: "375px",
+          }}
+        >
+          <tbody>
+            <BoardRow></BoardRow>
+            <BoardRow isCenterRow={true}></BoardRow>
+            <BoardRow></BoardRow>
+          </tbody>
+        </table>
+      </TicTacToeContext.Provider>
+    );
+  }
 }
 
 export default Board;
--- src/components/boardPiece.jsx
+++ src/components/boardPiece.jsx
@@ -1,65 +1,51 @@
-import React, { Component } from 'react';
-import TicTacToeContext from '../contexts/ticTacToeContext'
+import React, { Component } from "react";
+import TicTacToeContext from "../contexts/ticTacToeContext";
 
 class BoardPiece extends Component {
-    state = {
-        isCenter: false,
-        isCenterRow: false,
-        boardPieceValue: "_"
+  state = {
+    isCenter: false,
+    isCenterRow: false,
+    boardPieceValue: "_",
+  };
+
+  componentDidMount() {
+    let { isCenter, isCenterRow } = this.props;
+    if (isCenter === undefined) {
+      isCenter = false;
+    } else {
+      isCenter = true;
     }
 
-    componentDidMount() {
-        let { isCenter, isCenterRow } = this.props;
-        if (isCenter === undefined) {
-            isCenter = false;
-        } else {
-            isCenter = true;
-        }
+    this.setState({ isCenter, isCenterRow });
+  }
 
-        // if (isCenterRow === undefined) {
-        //     isCenterRow = false;
-        // } else {
-        //     isCenterRow = true;
-        // }
+  changeBoardPieceValue = (context) => {
+    context.changeValue()
+    let { boardPieceValue } = this.state;
 
-
-        this.setState({ isCenter, isCenterRow })
+    if (boardPieceValue === "_") {
+      this.setState({ boardPieceValue: context.nextValue });
     }
+  };
 
-    xo(boardPieceValue) {
-        console.log(boardPieceValue)
-        if (boardPieceValue === "_") {
-            this.setState({ boardPieceValue: "x" })
-        }
-        else if (boardPieceValue === "x") {
-            this.setState({ boardPieceValue: "o" })
+  render() {
+    const centerClass = this.state.isCenter ? "v " : "";
+    const centerRowClass = this.state.isCenterRow ? "h" : "";
+    const { boardPieceValue } = this.state;
 
-
-        }
-        else if (boardPieceValue === "o") {
-            this.setState({ boardPieceValue: "_" })
-
-
-        }
-    }
-
-
-    render() {
-        const centerClass = this.state.isCenter ? "v " : "";
-        const centerRowClass = this.state.isCenterRow ? "h" : "";
-
-        return (
-            <TicTacToeContext.Consumer >
-                {value =>
-                    <td className={`square ${centerClass} ${centerRowClass}`}
-                        onClick={() => this.xo(value)}>{this.state.boardPieceValue}
-                    </td>
-                }
-
-            </TicTacToeContext.Consumer>
-        );
-
-    }
+    return (
+      <TicTacToeContext.Consumer>
+        {(context) => (
+          <td
+            className={`square ${centerClass} ${centerRowClass}`}
+            onClick={() => this.changeBoardPieceValue(context)}
+          >
+            {boardPieceValue}
+          </td>
+        )}
+      </TicTacToeContext.Consumer>
+    );
+  }
 }
 
 export default BoardPiece;
